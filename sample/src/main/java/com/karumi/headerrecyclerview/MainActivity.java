d4fea13a4559137ddc3cfc62f8c2f473c6fe7b67
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-05-18 11:46:35 +0200

    Add checkstyle configuration to the project

--- sample/src/main/java/com/karumi/headerrecyclerview/MainActivity.java
+++ sample/src/main/java/com/karumi/headerrecyclerview/MainActivity.java
@@ -1,11 +1,11 @@
 package com.karumi.headerrecyclerview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivityActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
