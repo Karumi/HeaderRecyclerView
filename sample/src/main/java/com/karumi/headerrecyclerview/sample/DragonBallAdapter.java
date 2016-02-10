ff7744c1e2acc1e518be0291937201cb59c0f0c8
cmicat <lonelycatme@gmail.com>
2016-02-10 09:08:01 +0800

    update DragonBallAdapter.java, according to the library code change

--- sample/src/main/java/com/karumi/headerrecyclerview/sample/DragonBallAdapter.java
+++ sample/src/main/java/com/karumi/headerrecyclerview/sample/DragonBallAdapter.java
@@ -20,7 +20,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.karumi.headerrecyclerview.HeaderRecyclerViewAdapter;
 import com.karumi.headerrecyclerview.sample.util.DebugHelper;
 
@@ -29,117 +28,62 @@ import com.karumi.headerrecyclerview.sample.util.DebugHelper;
  * characters.
  */
 public class DragonBallAdapter extends
-        HeaderRecyclerViewAdapter<RecyclerView.ViewHolder, DragonBallHeader, DragonBallCharacter,
-                DragonBallFooter> {
+    HeaderRecyclerViewAdapter<RecyclerView.ViewHolder, DragonBallHeader, DragonBallCharacter, DragonBallFooter> {
 
-    private static final String LOG_TAG = DragonBallAdapter.class.getSimpleName();
+  private static final String LOG_TAG = DragonBallAdapter.class.getSimpleName();
 
-    @Override
-    protected RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = getLayoutInflater(parent);
-        View headerView = inflater.inflate(R.layout.row_dragon_ball_header, parent, false);
-        return new HeaderViewHolder(headerView);
-    }
+  @Override
+  protected RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent, int viewType) {
+    LayoutInflater inflater = getLayoutInflater(parent);
+    View headerView = inflater.inflate(R.layout.row_dragon_ball_header, parent, false);
+    return new HeaderViewHolder(headerView);
+  }
 
-    @Override
-    protected RecyclerView.ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = getLayoutInflater(parent);
-        View characterView = inflater.inflate(R.layout.row_dragon_ball_character, parent, false);
-        return new CharacterViewHolder(characterView);
-    }
+  @Override
+  protected RecyclerView.ViewHolder onCreateItemViewHolder(ViewGroup parent, int viewType) {
+    LayoutInflater inflater = getLayoutInflater(parent);
+    View characterView = inflater.inflate(R.layout.row_dragon_ball_character, parent, false);
+    return new CharacterViewHolder(characterView);
+  }
 
-    @Override
-    protected RecyclerView.ViewHolder onCreateFooterViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = getLayoutInflater(parent);
-        View footerView = inflater.inflate(R.layout.row_dragon_ball_footer, parent, false);
-        return new FooterViewHolder(footerView);
-    }
+  @Override
+  protected RecyclerView.ViewHolder onCreateFooterViewHolder(ViewGroup parent, int viewType) {
+    LayoutInflater inflater = getLayoutInflater(parent);
+    View footerView = inflater.inflate(R.layout.row_dragon_ball_footer, parent, false);
+    return new FooterViewHolder(footerView);
+  }
 
-    @Override
-    protected void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
-        DragonBallHeader header = getHeader();
-        HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
-        headerViewHolder.render(header);
-    }
+  @Override protected void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+    DragonBallHeader header = getHeader();
+    HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+    headerViewHolder.render(header);
+  }
 
-    @Override
-    protected void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position) {
-        DragonBallCharacter character = getItem(position);
-        CharacterViewHolder characterViewHolder = (CharacterViewHolder) holder;
-        characterViewHolder.render(character);
-    }
+  @Override protected void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position) {
+    DragonBallCharacter character = getItem(position);
+    CharacterViewHolder characterViewHolder = (CharacterViewHolder) holder;
+    characterViewHolder.render(character);
+  }
 
-    @Override
-    protected void onBindFooterViewHolder(RecyclerView.ViewHolder holder, int position) {
-        DragonBallFooter footer = getFooter();
-        FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
-        footerViewHolder.render(footer);
-    }
+  @Override protected void onBindFooterViewHolder(RecyclerView.ViewHolder holder, int position) {
+    DragonBallFooter footer = getFooter();
+    FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
+    footerViewHolder.render(footer);
+  }
 
-    private LayoutInflater getLayoutInflater(ViewGroup parent) {
-        return LayoutInflater.from(parent.getContext());
-    }
+  private LayoutInflater getLayoutInflater(ViewGroup parent) {
+    return LayoutInflater.from(parent.getContext());
+  }
 
-    @Override
-    protected boolean onFailedToRecycleHeaderView(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-        return super.onFailedToRecycleHeaderView(holder);
-    }
+  @Override protected void onHeaderViewRecycled(RecyclerView.ViewHolder holder) {
+    DebugHelper.logMethod(LOG_TAG);
+  }
 
-    @Override
-    protected boolean onFailedToRecycleItemView(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-        return super.onFailedToRecycleItemView(holder);
-    }
+  @Override protected void onItemViewRecycled(RecyclerView.ViewHolder holder) {
+    DebugHelper.logMethod(LOG_TAG);
+  }
 
-    @Override
-    protected boolean onFailedToRecycleFooterView(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-        return super.onFailedToRecycleFooterView(holder);
-    }
-
-    @Override
-    protected void onHeaderViewAttachedToWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onItemViewAttachedToWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onFooterViewAttachedToWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onHeaderViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onItemViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onFooterViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onHeaderViewRecycled(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onItemViewRecycled(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
-
-    @Override
-    protected void onFooterViewRecycled(RecyclerView.ViewHolder holder) {
-        DebugHelper.logMethod(LOG_TAG);
-    }
+  @Override protected void onFooterViewRecycled(RecyclerView.ViewHolder holder) {
+    DebugHelper.logMethod(LOG_TAG);
+  }
 }
